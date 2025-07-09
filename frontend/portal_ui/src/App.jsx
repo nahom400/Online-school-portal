942b66efd6aaa8c40509bbf479689e6beaa923cd
Nahom <nahummisge@gmail.com>
2025-07-09 19:56:48 +0300

    Refactored SignBox into the app.
    Now the app knows who's new and who has logged in before.
    
    On branch master
    Changes to be committed:
    	modified:   frontend/portal_ui/src/App.jsx
    	modified:   frontend/portal_ui/src/components/SignBox.jsx

--- frontend/portal_ui/src/App.jsx
+++ frontend/portal_ui/src/App.jsx
@@ -1,17 +1,40 @@
+import { useState } from 'react'
+import useSWR from 'swr'
 import Entry from './Entry'
 import SignBox from './components/SignBox'
 import './styles/bootstrap.min.css'
 import './index.css'
 
+const BASE_URL = "http://127.0.0.1:8000/"
+const REQUEST_URL = "auth/get_token/"
+
+const fetcher = (...args) => fetch(...args).then((res)=>(res.json()))
+
 function App(){
 
+	const [myToken, setMyToken] = useState(localStorage.getItem("ACCESS_TOKEN"))
+	const [fullUrl, setFullUrl] = useState(null)
+
+	function signInAction(formData){
+		const username = formData.get('username')
+		const password = formData.get('password')
+		console.log("Signin action from app.jsx")
+		const FULL_URL = BASE_URL + REQUEST_URL + `?username=${username}&password=${password}`
+		setFullUrl(FULL_URL)
+
+		if (response){
+			localStorage.setItem("ACCESS_TOKEN",response.token)
+			localStorage.setItem("REFRESH_TOKEN",response.token_refresh)
+			setMyToken(response.token)	
+		}
+	}
+
+	const {data:response, isValidating, error} = useSWR(fullUrl, fetcher)
 	return(
 		<>
 			{/*<h1 class="">This is the portal for Teachers and Students </h1>
-			<p>Please sign-in: if you're a teacher it takes you to the teacher's portal and if you're a student it takes you to the students portal</p>
-			<SignBox />*/}
-
-			<Entry />
+			<p>Please sign-in: if you're a teacher it takes you to the teacher's portal and if you're a student it takes you to the students portal</p>*/}
+			{ myToken ? <Entry /> :  <SignBox signInAction={signInAction}/>}
 		</>
 		)
 }
--- frontend/portal_ui/src/components/SignBox.jsx
+++ frontend/portal_ui/src/components/SignBox.jsx
@@ -1,51 +1,21 @@
 import useSWR from 'swr'
 import { useState } from 'react'
 
-const fetcher = (...args) => fetch(...args).then((res)=>(res.json()))
 
-const BASE_URL = "http://127.0.0.1:8000/"
-const REQUEST_URL = "auth/get_token/"
+function SignBox({signInAction}){
 
-function SignBox(){
+	return (
+		<form action={signInAction} className="SignBox">
 
-	const [fullUrl, setFullUrl] = useState(null)
-	const [myToken, setMyToken] = useState(localStorage.getItem("ACCESS_TOKEN"))
-
-	function signInAction(formData){
-		const username = formData.get('username')
-		const password = formData.get('password')
-		const FULL_URL = BASE_URL + REQUEST_URL + `?username=${username}&password=${password}`
-		setFullUrl(FULL_URL)
-		if (response){
-		localStorage.setItem("ACCESS_TOKEN",response.token)
-		localStorage.setItem("REFRESH_TOKEN",response.token_refresh)
-		setMyToken(response.token)	
-		}
-	}
-	const {data:response, isValidating, error} = useSWR(fullUrl, fetcher)
-
-	function tokenCheckAction(){
-	}
-
-	if (!myToken){
-		return (
-			<form action={signInAction} className="SignBox">
-
-				<label> Username:  
-					<input name="username" className="input" type="text" /> 
-				</label>
-				<label> Password: 
-					<input name="password" className="input" type="password" />
-				</label>
-				<button type="submit"> Go </button>
-				{ response && response.error ? <p>Error: {response.error}</p> : null }
-			</form>
-		)
-	}
-	
-	else if (myToken){
-		return(<p>TOKEN: {myToken}</p>)
-	}
+			<label> Username:  
+				<input name="username" className="input" type="text" /> 
+			</label>
+			<label> Password: 
+				<input name="password" className="input" type="password" />
+			</label>
+			<button> Go </button>
+		</form>
+	)
 }
 
 export default SignBox;
