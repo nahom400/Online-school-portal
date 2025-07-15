54a700d8beb0965af71dca32cb1d53e0245094cd
Nahom <nahummisge@gmail.com>
2025-07-15 22:09:01 +0300

    Improved Dashboard UI
    - The side bars now feel more like tab switchers

--- frontend/portal_ui/src/components/StudentDashboard.jsx
+++ frontend/portal_ui/src/components/StudentDashboard.jsx
@@ -5,15 +5,30 @@ import Account from "./Account"
 
 
 function StudentDashboard({username, token, profileData}){
-		const [page, setPage] = useState('Account')
+		const [page, setPage] = useState('account')
 		// const response = response.map(({subject_name, mark, date_recorded})=>({subject_name, mark, date_recorded, }))
 		// console.log(scores)
 		return (<div className="d-flex gap-2">
-				<div className="d-flex flex-column align-items-start">
-					<button className="btn btn-lg" onClick={()=>(setPage('account'))}>Account</button>
-					<button className="btn btn-lg">What's on the table?</button>
-					<button className="btn btn-lg" onClick={()=>(setPage('scores'))}>Scores</button>
-					<button className="btn btn-lg">Help</button>
+				<div className="d-flex flex-column justify-content-center shadow">
+
+					<button 
+						className={"w-100 btn btn-lg  " + (page==='account' ? 'bg-white': 'bg-transparent')} 
+						onClick={()=>(setPage('account'))}>
+						Account
+					</button>
+					<button 
+						className={"w-100 btn btn-lg " + (page==='table' ? 'bg-white': 'bg-transparent')} >
+						What's on the table?
+					</button>
+					<button 
+						className={"w-100 btn btn-lg " + (page==='scores' ? 'bg-white': 'bg-transparent')} 
+						onClick={()=>(setPage('scores'))}>
+						Scores
+					</button>
+					<button 
+						className={"w-100 btn btn-lg " + (page==='help' ? 'bg-white': 'bg-transparent')} >
+						Help
+					</button>
 				</div>
 				{ page === 'account' ? <Account username={username} token={token} profileData={profileData}/> : null }
 				{ page === 'scores' ? <Scores username={username} token={token}/> : null }
